9340d94775d81cf457af4acaa230bc4d7ddfcc0e
shivalilapare00 <90516920+shivalilapare00@users.noreply.github.com>
2025-12-05 10:17:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function firstWord(s) {
+	s = s.trim();
+
+	
+  // Find the index of the first space
+  const spaceIndex = s.indexOf(' ');
+
+  // If no space found, return the whole string
+  if (spaceIndex === -1) {
+    return s;
+  }
+
+  // Return substring from start to first space
+  return s.substring(0, spaceIndex);
   // your code here
 }
 
